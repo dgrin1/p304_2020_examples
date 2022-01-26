9f6ce59b476f2f51ed220f17e75eac61d6dc6b4a
dgrin1 <dgrin314@gmail.com>
2022-01-26 10:53:43 -0500

    Hi class. Here are some examples for today

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+name_of_person=input("what is your name?")
+print("Hello,",name_of_person,"? How do you like teaching by zoom?")
---
+++ polar_conert.py
@@ -0,0 +1,21 @@
+#import numpy as np
+#from math import log, sin, cos, pi
+from numpy import log, sin, cos, pi
+
+#overkill in python 2 which can read in floats but necessary in python 3 which reads everything as a string initially
+
+
+Read in r and theta values
+r=float(input("What is the radius?"))
+th=float(input("What is the angle in degrees?"))
+#convert strings to floats
+
+#convert angle from degrees to radians
+th_rad=th *pi/180.
+
+# get x and y values with standard trig formulae
+x=r*cos(th_rad)
+y=r*sin(th_rad)
+
+#output results to user
+print("Cartesian coordinates x=",x,"y=",y)
