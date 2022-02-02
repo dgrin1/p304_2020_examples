31654306e4474713185e34d41ed234476d5d4b94
Grin <dgrin@cr-stokes004.quaker.haverford.edu>
2022-02-02 11:31:58 -0500

    added

--- libcheck.py
+++ libcheck.py
@@ -3,17 +3,19 @@ import math
 
 def gsin(x):
 #take arg mod 2pi
-	x=x%(2.e0*np.pi)
 #initialize iterator and sum
 	i = 0
-	s,sold = 0.e0,0.e0
+	s,sold = 0.e0,0.0001e0
 
 #Keep at most 10000 terms in the Taylor series
 	for i in range(10000):
-		sold=s
 		s+= float((((-1)**i) * (x**((2*i) + 1))))/float(math.factorial(((2*i) + 1)))
 #If converged to machine precision then break out of loop
-		if sold==s: break
+		continue_criterion=(((s-sold)/sold)>0.00000000001)
+		print(continue_criterion)
+		if (continue_criterion==0): break
+		sold=s
+
 	return s
 
 def fac(n):
