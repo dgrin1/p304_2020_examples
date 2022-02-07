513b4b10832040c2db201509e1a743a386978f1c
Grin <dgrin@cr-stokes004.quaker.haverford.edu>
2022-02-07 09:56:55 -0500

    inclass matplotlib examples

--- pcheck_sin.py
+++ pcheck_sin.py
@@ -15,7 +15,7 @@ y2=np.sin(x2)
 y3=np.sin(x+.4)
 
 plt.subplot(211)
-plt.title('Height of ocean swells'r'~\Gamma',fontsize=24,color='b')
+plt.title('Height of ocean swells'r'$~\Gamma$',fontsize=24,color='b')
 
 al=plt.plot(x,y,'k')
 bl=plt.plot(x2,y2,'r^',markersize=15)
@@ -34,7 +34,7 @@ plt.ylim([-1.1,1.1])
 
 
 plt.ylim([-1.3,1.3])
-plt.ylabel(r'\Gamma',fontsize=24)
-plt.xlabel(r'\omega t',fontsize=16)
+plt.ylabel(r'$\Gamma$',fontsize=24)
+plt.xlabel(r'$\omega t$',fontsize=16)
 plt.show()
 plt.savefig('ocean.pdf',format='pdf',dpi=1000)
