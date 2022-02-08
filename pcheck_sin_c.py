a124185e13a24051aae18af253a67527de48046e
dgrin1 <dgrin314@gmail.com>
2022-02-08 17:34:04 -0500

    lots of matplotlib examples for structure wed

--- pcheck_sin_c.py
+++ pcheck_sin_c.py
@@ -2,38 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 #plt.ion only works in interactive mode
 plt.ion()
+
+#set fonts
 plt.rc('text',usetex=True)
 plt.rc('font', **{'family':'sans-serif','sans-serif':['Helvetica']})
 plt.rc('font', **{'family':'serif','serif':['Times New Roman']})
 
-
+#create arrays
 x=np.linspace(0,2*np.pi,100)
 y=np.sin(x)
 x2=np.linspace(0,2*np.pi,15)
 y2=np.sin(x2)
 y3=np.sin(x+.4)
 
-plt.subplot(211)
-plt.title('Height of ocean swells'r'$~\Gamma$',fontsize=24,color='b')
-
+#make multiple plots on one panel
 al=plt.plot(x,y,'k')
-bl=plt.plot(x2,y2,'r^',markersize=15)
+bl=plt.plot(x2,y2,'r^',markersize=5)
+cl=plt.plot(x2,y2,'co',markersize=5)
+#formatting and size of symbols
 
-plt.setp(al,linewidth=4.)
+#linewidth
+plt.setp(al,linewidth=5)
 
+#limits
 plt.ylim([-1.1,1.1])
 
-plt.subplot(212)
-al=plt.plot(x,y3,'k')
-plt.setp(al,linewidth=4.)
-
-plt.plot(x2,y2,'r^',markersize=15)
-plt.ylim([-1.1,1.1])
-
-
-
-plt.ylim([-1.3,1.3])
+#labels in latex
 plt.ylabel(r'$\Gamma$',fontsize=24)
 plt.xlabel(r'$\omega t$',fontsize=16)
+
 plt.show()
+
+#nice output with set resolution
 plt.savefig('ocean.pdf',format='pdf',dpi=1000)
