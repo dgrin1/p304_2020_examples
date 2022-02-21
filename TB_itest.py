1385d51399bc20bf18614c02a5fc4471a9d49d7b
Theodore Bien <theo@theodores-mbp.clients.haverford.edu>
2022-02-21 10:35:16 -0500

    Theo Bien in-class integration work

--- TB_itest.py
+++ TB_itest.py
@@ -12,12 +12,15 @@ def f(x):
 	f=np.power((x-1),5.e0)-5.*x*(np.sin(5*x))
 	return f
 
+def intf(x):
+	intf = 1/6*(-1+x)**6 + x * np.cos(5*x)-1/5*np.sin(5*x)
+	return intf
+
 def reiman(a,b,f,N):
 	x=0
 	s=0
-	ivec=range(0,N+1)
 	h=float(b-a)/float(N)
-	for i in ivec:
+	for i in range(0,N+1):
 		s+=f(x)*h
 		x=a+i*h
 	return s
@@ -40,9 +43,9 @@ dif = [0]
 N_steps = 100
 
 for N_var in range(10,1000,10):
-	numer = reiman(a,b,f,N_var)
-	exact = trape(a,b,f,N_var)
-	val = (numer - exact)/exact
+	numer = trape(a,b,f,N_var)
+	exact = intf(b) - intf(a)
+	val = abs((numer - exact)/exact)
 	dif.append(val)
 
 dif.remove(0)
@@ -51,8 +54,26 @@ x = range(10,1000,10)
 
 plt.figure(1)
 plt.plot(x,y)
+plt.title("Plot of the numberical difference")
 
 plt.figure(2)
 plt.loglog(x,y)
+plt.title("Loglog plot of the numberical difference")
+
+dif2 = [0]
+
+for N_var2 in range(10,1000,10):
+	numer = trape(a,b,f,N_var2)
+	numer2 = trape(a,b,f,2*N_var2)
+	val = (numer2 - numer)/(numer)
+	dif2.append(val)
+
+dif2.remove(0)
+y2 = dif2
+x2 = x
+
+plt.figure(3)
+plt.loglog(x2,y2)
+plt.title("Loglog plot of the numberical difference")
 
 plt.show()
